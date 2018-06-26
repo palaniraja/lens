06014d2a5d68b09687a7582dbec188b693b86149
palaniraja <palaniraja@live.com>
2018-06-26 21:02:44 +0530

    v1.0

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Work-in-progress
+# Correct disortion with lensfun and imagemagick
 
-trying to automate lens correction with lensfun db
+Simple bash script to automate the [lens disortion](http://www.imagemagick.org/Usage/lens/) correction process. Idea was to use with [Termux](https://play.google.com/store/apps/details?id=com.termux&hl=en_IN) on my mobile.
 
+
+![command usage](screenshot.png)
+
+
+## Imagemagick command demo
 
 [![Youtube video](https://img.youtube.com/vi/Ihd6r9h-TO8/0.jpg)](https://www.youtube.com/watch?v=Ihd6r9h-TO8)
--- lens.sh
+++ lens.sh
@@ -2,7 +2,7 @@
 
 
 # Activities to perform
-# * Do lens disortion
+# * Do lens disortion - DONE
 # * Do Lens Color Aberration (LCA) - if applicable
 # * Do Vignetting - if applicable
 
@@ -30,6 +30,43 @@ lens55250="Canon EF-S 55-250mm f/4-5.6 IS II"
 lens50="Canon EF 50mm f/1.8 STM"
 
 
+
+# Function to return correct abc for each lens types
+
+lensParam=()
+
+function lensParam1855 () {
+    lensParam=()
+    lensParam["18"]="0.02504 -0.06883 0.01502"
+    lensParam["24"]="0.0203 -0.06514 0.04768"
+    lensParam["29"]="0.00854 -0.01505 -0.00639"
+    lensParam["35"]="-0.01582 0.04918 -0.05313"
+    lensParam["41"]="-0.00694 0.02694 -0.03595"
+    lensParam["46"]="-0.01908 0.07131 -0.08927"
+    lensParam["55"]="-0.00098 0.01803 -0.04704"
+}
+
+
+function lensParam55250 () {
+    lensParam=()
+    lensParam["55"]="-0.000303056 -0.00581537 -0.00237143"
+    lensParam["70"]="-0.000520255 0.0000286405 0.00286934"
+    lensParam["100"]="0.00268582 -0.00578304 0.00718748"
+    lensParam["135"]="0.00120309 0.00979416 -0.00868762"
+    lensParam["250"]="-0.00226069 0.0111545 -0.00536676"
+}
+
+
+function lensParam50 () {
+    lensParam=()
+    lensParam["50"]="0.0061844 -0.0313122 0.0314815"
+}
+
+# model=$lens50
+# flength="50.0 mm"
+
+
+
 # === Fetching Lens Model to search for lensfun ===
 # exiftool forig.jpg | grep "Lens ID" | awk -F': ' '{ print $2 }'
 
@@ -47,7 +84,6 @@ lens50="Canon EF 50mm f/1.8 STM"
 # to get focal length, -FocalLength because too many "Focal Length"
 
 
-
 echo "-------"
 
 model=`exiftool -s2  -LensID "$1" | awk -F': ' '{ print $2 }'`
@@ -58,17 +94,62 @@ echo "Focal Length: $flength"
 
 echo "-------"
 
+
+
+intFocal=`echo "$flength" | cut -d "." -f1`
+isValidLens=0
+
 if [ "$model" == "$lens1855" ]
 then
+    isValidLens=1
     echo "It is a 18-55mm lens @ $flength"
+    lensParam1855
 fi
 
 if [ "$model" == "$lens50" ]
 then
+    isValidLens=1
     echo "It is a 50mm lens @ $flength"
+    lensParam50
 fi
 
 if [ "$model" == "$lens55250" ]
 then
+    isValidLens=1
     echo "It is a 55-250mm lens @ $flength"
+    lensParam55250
 fi
+
+if [ "$isValidLens" -eq "0" ]
+then
+    echo "model($model) is not configured in script"
+    exit 0
+fi
+
+pFocal=0
+valABC="0,0,0"
+
+for K in "${!lensParam[@]}";do 
+    if [ "$K" -le "$intFocal"  ]; then
+        # saving previous focal value
+        pFocal=$K
+        
+    else 
+        # focal value greater than $K; use previous
+        break
+    fi
+done
+
+valABC=`echo "${lensParam[$pFocal]}"`
+echo "Param for ptLens=$pFocal to be used ... ($valABC)"
+
+# fetch just the params from a,b,c
+# a=` echo "$valABC" | awk -F, '{ print $1 }' `
+# b=` echo "$valABC" | awk -F, '{ print $2 }' `
+# c=` echo "$valABC" | awk -F, '{ print $3 }' `
+# echo "a=$a, b=$b, c=$c"
+echo -n "Processing with imagemagick ..."
+convert $1 -distort barrel "$valABC" "corrected-$1"
+echo " [ DONE ]"
+echo "Output file: corrected-$1"
+exit 0
